89f1728004c39b993d6666e39ff09a1885fe4bf8
Vlad Baydan <@email>
2026-07-31 00:30:18 +0100

    build: drop unused stargate feature, pin rust-version

--- src/bin/schema.rs
+++ src/bin/schema.rs
@@ -1,11 +1,12 @@
 use cosmwasm_schema::write_api;
 
-use oracle_score::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use oracle_score::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
         execute: ExecuteMsg,
         query: QueryMsg,
+        migrate: MigrateMsg,
     }
 }
